7bd8f053deb10c41e07df7da5ec00e88bfb4dc74
Nathan Petersen <npetersen2@wisc.edu>
2020-07-27 18:09:10 -0500

    Working LEDs

--- Motherboard/Firmware/motherboard_v1/Core/Src/drv_led.c
+++ Motherboard/Firmware/motherboard_v1/Core/Src/drv_led.c
@@ -54,10 +54,11 @@ void drv_led_toggle(uint8_t pattern)
 
 static inline void drv_led_write(uint8_t x)
 {
-    GPIO_SET_PIN(GPIOD, GPIO_PIN_0, (x & (1 << 0)));
-    GPIO_SET_PIN(GPIOD, GPIO_PIN_1, (x & (1 << 1)));
-    GPIO_SET_PIN(GPIOD, GPIO_PIN_2, (x & (1 << 2)));
-    GPIO_SET_PIN(GPIOD, GPIO_PIN_3, (x & (1 << 3)));
+    // LEDs are active LOW
+    GPIO_SET_PIN(GPIOB, GPIO_PIN_5, (x & (1 << 0)) == 0 ? 1 : 0);
+    GPIO_SET_PIN(GPIOB, GPIO_PIN_6, (x & (1 << 1)) == 0 ? 1 : 0);
+    GPIO_SET_PIN(GPIOB, GPIO_PIN_7, (x & (1 << 2)) == 0 ? 1 : 0);
+    GPIO_SET_PIN(GPIOB, GPIO_PIN_8, (x & (1 << 3)) == 0 ? 1 : 0);
 }
 
 void drv_led_display(void)
@@ -69,15 +70,15 @@ static void MX_LED_Init(void)
 {
     GPIO_InitTypeDef GPIO_InitStruct = { 0 };
 
-    __HAL_RCC_GPIOD_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
 
     // Configure GPIO pin Output Level
-    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8, GPIO_PIN_RESET);
 
     // Configure GPIO pins
-    GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3;
+    GPIO_InitStruct.Pin = GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 }
