76f492d1afe5fed15043b11c29d5d1e279386fbc
Patrick Nowakowski <pnowako123@gmail.com>
2024-08-30 16:44:36 -0500

    Replace ping-pong adc data buffers with single buffer

--- Mainboard/Firmware/motherboard_v1/Core/Src/adc.c
+++ Mainboard/Firmware/motherboard_v1/Core/Src/adc.c
@@ -40,12 +40,8 @@ static void setup_pin_CONVST(void);
 
 #define ADC_BITS_TO_VOLTS(bits) (ADC_VOLTS_PER_BIT * (float) bits)
 
-// As the code runs, these state variables are updated in ISRs
-// Ping-pong buffer of latest samples
-static volatile uint8_t read_buffer_number = 0;
-static volatile uint8_t write_buffer_number = 1;
-static volatile uint16_t latest_valid_adc_data0[8] = { 0 };
-static volatile uint16_t latest_valid_adc_data1[8] = { 0 };
+// Buffer of latest samples
+static volatile uint16_t latest_valid_adc_data[8] = { 0 };
 
 void adc_init(void)
 {
@@ -57,18 +53,9 @@ void adc_init(void)
 }
 
 // NOTE: this function is called from the transmit function
-//
-// To prove correctness, we just need to verify the invariant
-// that the `read_buffer_number` variable and corresponding buffer
-// always contain valid data.
 void adc_latest_bits(uint16_t *output)
 {
-    volatile uint16_t *data = NULL;
-    if (read_buffer_number == 0) {
-        data = latest_valid_adc_data0;
-    } else {
-        data = latest_valid_adc_data1;
-    }
+    volatile uint16_t *data = latest_valid_adc_data;
 
     // Give user their data (unrolled for speed)
     output[0] = data[0];
@@ -164,12 +151,7 @@ void EXTI3_IRQHandler(void)
     adc_sample_all_daughtercards(new_data);
 
     // Copy data into write buffer destination
-    volatile uint16_t *dest = NULL;
-    if (write_buffer_number == 0) {
-        dest = latest_valid_adc_data0;
-    } else {
-        dest = latest_valid_adc_data1;
-    }
+    volatile uint16_t *dest = latest_valid_adc_data;
 
     // Unrolled loop for speed
     dest[0] = new_data[0];
@@ -180,15 +162,6 @@ void EXTI3_IRQHandler(void)
     dest[5] = new_data[5];
     dest[6] = new_data[6];
     dest[7] = new_data[7];
-
-	// Switch read buffer to where we just put the new data,
-	// therefore, satisfying the property that the read_buffer_number
-	// always points to a valid set of samples!
-	read_buffer_number = 1 - read_buffer_number;
-
-	// Now that read buffer is set, we are safe to update write buffer
-	// for the next time this ISR runs...
-	write_buffer_number = 1 - write_buffer_number;
 
     // Call the function in tx.c to transmit the sampled data back to the AMDC
     transmit_samples();
