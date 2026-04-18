df568e7191175891ac12697930c344d78369d841
Eric Severson <sever@umn.edu>
2026-04-18 12:20:11 -0500

    Optimize DMA routing time for ST MCU

--- Mainboard/Firmware/motherboard_v1/Core/Src/adc.c
+++ Mainboard/Firmware/motherboard_v1/Core/Src/adc.c
@@ -155,6 +155,8 @@ void EXTI3_IRQHandler(void)
 {
 	// alert daisy chained AMDSs to begin converting
 	GPIO_TOGGLE_PIN(GPIOD, GPIO_PIN_1);
+    //reset DMA routing state machine to ensure robust operation in case bytes were dropped
+    try_reset_routing_state();
 	// Perform the actual SPI transactions
 	uint16_t new_data[8] = { 0 };
     adc_sample_all_daughtercards(new_data);
@@ -189,7 +191,8 @@ void EXTI3_IRQHandler(void)
 		}
 	}
 
-	process_routing_old();
+    //Handle any DMA data that has been received from daisy chain
+	try_process_routing(); // This try function is thread safe
 
     // Clear all pending IRQs for ADC conversions at the
     // end of this ISR so that the system realigns the
